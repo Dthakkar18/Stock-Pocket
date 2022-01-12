7317ee06458ac11b7a9dd1996cb9509351799d8e
Dthakkar18 <91099584+Dthakkar18@users.noreply.github.com>
2022-01-11 18:41:59 -0600

    Update
    - trying to make landing page look better

--- people/forms.py
+++ people/forms.py
@@ -5,23 +5,28 @@ from django.contrib.auth.forms import UserCreationForm, UsernameField
 
 User = get_user_model()
 
+# not using this form (created my own custom one)
 class addStockForm(forms.Form):
     company_name = forms.CharField()
     company_ticker = forms.CharField()
     shares = forms.FloatField()
     avg_share_price = forms.FloatField()
 
+# not using this form (created my own custom one)
 class updateStockForm(forms.Form):
     company_ticker = forms.CharField()
     shares = forms.FloatField()
     avg_share_price = forms.FloatField()
 
+# not using this form (created my own custom one)
 class deleteStockForm(forms.Form):
     company_ticker = forms.CharField()
 
+# not using this form (created my own custom one)
 class detailStockForm(forms.Form):
     company_ticker = forms.CharField()
 
+# not using this form (created my own custom one)
 class loginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField()
